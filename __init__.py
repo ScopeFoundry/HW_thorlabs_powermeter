198f96129f8064a86993d71fe57ef1c98397a313
Edward Barnard <esbarnard@lbl.gov>
2017-01-10 12:19:28 -0800

    License and readme additions

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
 from .thorlabs_powermeter import ThorlabsPowerMeterHW 
-from .powermeter_optimizer_new import PowerMeterOptimizerMeasure
+from ScopeFoundryHW.thorlabs_powermeter.powermeter_optimizer import PowerMeterOptimizerMeasure
